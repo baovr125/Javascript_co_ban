27e2073da59a7fd7559a0346adf5641196e9138d
Bao Hoc Hoi <baozzskt2k4@gamil.com>
2023-12-14 10:35:18 +0700

    ok

---
+++ video7.js
@@ -0,0 +1,9 @@
+console.log("Helo World from HTML")
+let obj = {
+    name: `Bao` ,
+    address: `Nghe An`
+};
+let b = `Bao`;
+let c = ``;
+console.log(`What is your name ?`, `My name 's: `, obj.name)
+console.log(`Type of a:`, typeof a,`type of b`, typeof b)
---
+++ video9.js
@@ -0,0 +1,17 @@
+console.log(`Helo World from HTML`)
+let a = {
+    name: `Bao`,
+    age: `19`
+};
+console.log(`check data object: a`, a)
+console.log(`my name\`s`, a.name)
+// arr = [item1, item2, item 3];
+let b = [`la phong`, `thu hha`, ` lien minh`] ;
+console.log(`tyepe of b:`, typeof b,b)
+console.log(`first item`, b[0])
+let ok = [
+    {name: `pk`, age: 20},
+    {name: `ok`, age: 21}
+]
+
+console.log(` my family: `, ok)
